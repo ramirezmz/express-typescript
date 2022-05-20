6146de0342721e7a115ba00fe54ac571e5107a47
ramirezmz <roberto.paolo@dremio.com>
2022-05-20 09:48:54 -0300

    add: types for diaries + create diaryServices

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,12 @@ app.use(express.json()) // middleware que transforma la req.body a un JSON
 const PORT = 3000
 
 app.get('/ping', (_req, res) => {
-   console.log('Someone pinged here!!')
-   res.send('pong')
+  console.log('Someone pinged here!!')
+  res.send('pong')
 })
 
 app.use('/api/diaries', diaryRoutes)
 
 app.listen(PORT, () => {
-   console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`)
 })
--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,13 +1,31 @@
 import express from 'express'
+import * as diaryServices from '../services/diaryServices'
 
 const router = express.Router()
 
 router.get('/', (_req, res) => {
-   res.send('Fetching all entry here!!')
+  res.send(diaryServices.getEntriesWithoutSensitiveInfo())
 })
 
-router.post('/', (_req, res) => {
-   res.send('Saving a diary!!')
+router.get('/:id', (req, res) => {
+  const diary = diaryServices.findById(Number(req.params.id))
+  //   res.send(diary?.weather)
+  return (diary != null)
+    ? res.send(diary)
+    : res.sendStatus(404)
+})
+
+router.post('/', (req, res) => {
+  const { date, weather, visibility, comment } = req.body
+
+  const newDiaryEntry = diaryServices.addDiary({
+    date,
+    weather,
+    visibility,
+    comment
+  })
+
+  res.json(newDiaryEntry)
 })
 
 export default router
